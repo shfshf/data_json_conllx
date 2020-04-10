c0fcf38dc2db2aa52e400b8ccdaf1e6158938f8c
Xiaoquan Kong <u1mail2me@gmail.com>
2020-04-10 14:42:31 +0800

    fix: collect tag

--- data_json_conllx/collect_tag.py
+++ data_json_conllx/collect_tag.py
@@ -6,4 +6,4 @@ from tokenizer_tools.tagset.offset.corpus import Corpus
 corpus = Corpus.read_from_file("data/all_data.conllx")
 
 with open("data/final/entity.txt", "wt") as fd:
-    fd.write("\n".join(corpus.annotation_tags()))
+    fd.write("\n".join({span.entity for doc in corpus for span in doc.span_set}))
